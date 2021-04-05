bb23a40decf0e9f26c5a32a974e6b35dca62ff46
ecastan960 <ecastan1@eafit.edu.co>
2021-04-05 12:41:57 -0500

    02|Removing unnecesary lines

--- 0x0B-ssh/100-puppet_ssh_config.pp
+++ 0x0B-ssh/100-puppet_ssh_config.pp
@@ -1,17 +1,5 @@
 #Config client ssh file to conect to server without password
-file_line { 'ssh_config':
-
-path => '/etc/ssh/ssh_config',
-line => 'Include /etc/ssh/ssh_config',
-}
-
-file_line { 'Host':
-
-path => '/etc/ssh/ssh_config',
-line => 'Host *',
-}
-
-file_line { 'holberton':
+file_line { 'key':
 
 path => '/etc/ssh/ssh_config',
 line => 'IdentityFile ~/.ssh/holberton',
@@ -21,19 +9,4 @@ file_line { 'PasswordAuthentication':
 
 path => '/etc/ssh/ssh_config',
 line => 'PasswordAuthentication no',
-}
-file_line { 'HashKnownHosts':
-
-path => '/etc/ssh/ssh_config',
-line => 'HashKnownHosts yes',
-}
-file_line { 'HashKnownHosts':
-
-path => '/etc/ssh/ssh_config',
-line => 'GSSAPIAuthentication yes',
-}
-file_line { 'SendEnv':
-
-path => '/etc/ssh/ssh_config',
-line => 'SendEnv LANG LC_*',
 }
