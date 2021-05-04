9e323411e957ed304f957456b685964e493adb55
ecastan960 <ecastan1@eafit.edu.co>
2021-05-04 00:42:24 -0500

    01|Update file

--- 0x15-api/0-gather_data_from_an_API.py
+++ 0x15-api/0-gather_data_from_an_API.py
@@ -8,7 +8,20 @@ import sys
 
 if __name__ == "__main__":
     user_id = sys.argv[1]
+    t_done = 0
     users_url = 'https://jsonplaceholder.typicode.com/users/{}'
-    response = requests.get(users_url.format(user_id))
-    name = response.json()['name']
-    print("Employee {} is done with tasks({}/{}):".format(name, 8, 20))
+    tasks_url = 'https://jsonplaceholder.typicode.com/todos?userId={}'
+    response_user = requests.get(users_url.format(user_id))
+    response_task1 = requests.get(tasks_url.format(user_id))
+    response_task2 = requests.get(tasks_url.format(user_id))
+    name = response_user.json()['name']
+    t_total = len(response_task1.json())
+    for tasks in range(t_total):
+        if response_task1.json()[tasks]['completed'] is True:
+            t_done += 1
+    text = "Employee {} is done with tasks({}/{}):"
+    print(text.format(name, t_done, t_total))
+    for tasks in range(t_total):
+        if response_task1.json()[tasks]['completed'] is True:
+            task = response_task1.json()[tasks]['title']
+            print("\t {}".format(task))
