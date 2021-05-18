8b26b98b7f5705f1943766b7232c331622811f26
ecastan960 <ecastan1@eafit.edu.co>
2021-05-17 19:44:30 -0500

    00|First test

---
+++ 0x16-api_advanced/0-subs.py
@@ -0,0 +1,23 @@
+#!/usr/bin/python3
+"""Python script that uses a reddit API
+"""
+import json
+import requests
+
+
+def number_of_subscribers(subreddit):
+    """Funtion that returns the number of
+    subscribers if exists
+    """
+    url = "https://www.reddit.com/r/{}/about.json"
+    headers = {
+        'User-Agent': 'My User Agent 1.0',
+        'From': 'youremail@domain.com'
+    }
+    r_subs = requests.get(url.format(subreddit), headers=headers)
+    if r_subs.status_code == 200:
+        data = r_subs.json()['data']
+        subscribers = data.get('subscribers')
+        if subscribers is not None:
+            return subscribers
+    return 0
